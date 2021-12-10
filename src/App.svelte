52c0d1509230dda25fb8c43d2da166c780514cbf
tututwo <tugordon@outlook.com>
2021-12-10 15:01:50 -0700

    data

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Chart from "./Chart.svelte"
+</script>
+
+<main>
+  <section class = "title">
+    <h1>Svelte Heatmap</h1>
+  </section>
+
+	<Chart />
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		margin: 0 auto;
+    font-family: "Inter", sans-serif;
+    padding: 0 1em;
+    max-width: 50em;
+	}
+
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+</style>
---
+++ src/Chart.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Rects from "./components/Rects.svelte"
+
+</script>
+
+
+<figure>
+  <div class = "chart-container">
+    <Rects />
+  </div>
+</figure>
+
+
+<style>
+  figure {
+    display: block;
+    position: relative;
+    width: 100%;
+    height: 70vh;
+    min-height: 400px;
+    margin: 0;
+    user-select: none;
+  }
+
+  figure > * {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+</style>
